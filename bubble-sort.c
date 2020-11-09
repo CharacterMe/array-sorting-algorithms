03079fd11b84fc7bda9034c9dd23dc17f0821711
Stefan Moldoveanu <characterme1001@gmail.com>
2020-11-09 09:23:34 +0200

    Add Bubble Sort Skeleton

---
+++ bubble-sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void sort(int* arr){
+    ///TODO: complete algorithm
+}
+
+int main(){
+    int n;
+    int* arr;
+
+    ///TODO: read n and array and call sort function
+
+    return 0;
+}
